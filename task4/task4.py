77834490ae85722a162d045f1d156054c15a31ea
Merana <155893896+M3RZL9@users.noreply.github.com>
2024-04-16 17:27:29 +0400

    Update task4.py

--- task4/task4.py
+++ task4/task4.py
@@ -13,7 +13,8 @@ def min_operations(nums):
                     
     
 if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print("How to use: python3 task4.py <file_with_circle_info> <file_with_points_info>")
+        sys.exit(1)
     nums = parse_file(sys.argv[1])
-    print(nums)
     print(min_operations(nums))
-    print(nums)
